dc50b9c3ce5ba274bd187368e424c4ff3a93ac0e
Eliezer <eliezer1789j@gmail.com>
2020-01-27 19:30:56 +0000

    usando variavel de referencia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nome = 'Eliezer';
 
-  adicionar() {
-    console.log(`Adicionando ${this.nome}`);
-
-    const numero = Math.round(Math.random() * 100);
-    this.nome = 'João ' + numero;
-  }
-
-  alterarNome(event: any) {
-    // console.log(event);
-    this.nome = event.target.value;
+  adicionar(nome: any) {
+   this.nome = nome;
   }
 
 }
