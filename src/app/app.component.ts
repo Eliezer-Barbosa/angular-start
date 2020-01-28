adb8af0438648430dd73112bfc43d6decd7fe70f
Eliezer <eliezer1789j@gmail.com>
2020-01-28 17:36:10 +0000

    Binding de eventos customizados com @Output e EventEmitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  ultimoId = 0;
-  nome = 'Eliezer';
-  adicionado = false;
+
   funcionarios = [];
 
-  adicionar() {
-    console.log(`Adicionando ${this.nome}`);
-    this.adicionado = true;
-
-    this.funcionarios.push({
-      id: ++this.ultimoId,
-      nome: this.nome
-    });
+  aoAdicionar(funcionario) {
+    this.funcionarios.push(funcionario);
   }
+
 }
